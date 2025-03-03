234e4814c2d32c5ff25ac45ca54fcc5a1f51acf6
Francesco <120659351+Tonno6@users.noreply.github.com>
2025-03-03 16:48:59 +0100

    modified:   BuildWebGL/Build/BuildWebGL.data.unityweb 	modified:   BuildWebGL/Build/BuildWebGL.framework.js.unityweb 	modified:   BuildWebGL/Build/BuildWebGL.loader.js 	modified:   BuildWebGL/Build/BuildWebGL.wasm.unityweb 	deleted:    BuildWebGL/Nuovo Documento di testo.js 	modified:   BuildWebGL/index.html 	modified:   BuildWebGL/manifest.json 	new file:   BuildWebGL/service-worker.js

---
+++ BuildWebGL/service-worker.js
@@ -0,0 +1,24 @@
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('pwa-cache').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/Build/UnityLoader.js',
+        '/Build/Build.json',
+        '/Build/yourgame.js',
+        '/Build/yourgame.wasm',
+        '/icon-192x192.png',
+        '/icon-512x512.png',
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
